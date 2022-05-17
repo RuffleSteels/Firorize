26c635caa8acb5025cf53858e09e70ede985b815
Lollypoplove08 <rafferty.smith@hotmail.com>
2022-05-17 22:57:01 +0200

    1.18.2 Update

---
+++ src/main/java/com/oscimate/oscimate_soulflame/OnSoulFireAccessor.java
@@ -0,0 +1,8 @@
+package com.oscimate.oscimate_soulflame;
+
+public interface OnSoulFireAccessor {
+    boolean isRenderSoulFire();
+
+    void setRenderSoulFire(boolean renderSoulFire);
+}
+
---
+++ src/main/java/com/oscimate/oscimate_soulflame/mixin/fire_overlays/client/ClientEntityMixin.java
@@ -0,0 +1,23 @@
+package com.oscimate.oscimate_soulflame.mixin.fire_overlays.client;
+
+
+import com.oscimate.oscimate_soulflame.OnSoulFireAccessor;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(Entity.class)
+public class ClientEntityMixin implements OnSoulFireAccessor {
+    @Unique
+    private boolean renderSoulFire;
+
+    @Override
+    public boolean isRenderSoulFire() {
+        return renderSoulFire;
+    }
+
+    @Override
+    public void setRenderSoulFire(boolean renderSoulFire) {
+        this.renderSoulFire = renderSoulFire;
+    }
+}
