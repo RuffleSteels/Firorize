51df78524f03b6283cf14bf640a40d58064a2671
Lollypoplove08 <rafferty.smith@hotmail.com>
2022-05-28 21:28:04 +0200

    Fixed the FireLogic bugs

--- src/main/java/com/oscimate/oscimate_soulflame/Main.java
+++ src/main/java/com/oscimate/oscimate_soulflame/Main.java
@@ -6,10 +6,14 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.SoulFireBlock;
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Util;
+import net.minecraft.util.crash.CrashException;
+import net.minecraft.util.crash.CrashReport;
+import net.minecraft.util.crash.CrashReportSection;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import org.checkerframework.checker.units.qual.C;
@@ -27,33 +31,58 @@ public class Main implements ClientModInitializer {
             this.CONFIG_MANAGER.save();
         }
         System.out.println(this.CONFIG_MANAGER.getStartupConfig());
-//        ClientTickEvents.START_CLIENT_TICK.register((client) -> {
-//            if (client.world != null) {
-//                client.world.getEntities().forEach(entity -> {
-//                    if (entity.isInLava()) {
-//                        ((OnSoulFireAccessor) entity).setRenderSoulFire(false);
-//                        return;
-//                    }
-//                    if(CONFIG_MANAGER.getCurrentFireLogic() == FireLogic.CONSISTENT) {
-//                        Box box = entity.getBoundingBox();
-//                        BlockPos.Mutable mutable = new BlockPos.Mutable();
-//                        BlockPos blockPos = new BlockPos(box.minX + 0.001D, box.minY + 0.001D, box.minZ + 0.001D);
-//                        BlockPos blockPos2 = new BlockPos(box.maxX - 0.001D, box.maxY - 0.001D, box.maxZ - 0.001D);
-//
-//                        for (int i = blockPos.getX(); i <= blockPos2.getX(); ++i) {
-//                            for (int j = blockPos.getY(); j <= blockPos2.getY(); ++j) {
-//                                for (int k = blockPos.getZ(); k <= blockPos2.getZ(); ++k) {
-//                                    mutable.set(i, j, k);
-//                                    if (client.world.getBlockState(mutable).getBlock() != Blocks.SOUL_FIRE) {
-//                                        ((OnSoulFireAccessor) entity).setRenderSoulFire(false);
-//                                        return;
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                });
-//            }
-//        });
+        ClientTickEvents.START_CLIENT_TICK.register((client) -> {
+            if (client.world != null) {
+                client.world.getEntities().forEach(entity -> {
+                    Box box = entity.getBoundingBox();
+                    BlockPos blockPos = new BlockPos(box.minX + 0.001, box.minY + 0.001, box.minZ + 0.001);
+                    BlockPos blockPos2 = new BlockPos(box.maxX - 0.001, box.maxY - 0.001, box.maxZ - 0.001);
+                    if (entity.world.isRegionLoaded(blockPos, blockPos2)) {
+                        BlockPos.Mutable mutable = new BlockPos.Mutable();
+                        Boolean isSoulFire = false;
+                        outerLoop:
+                        for (int i = blockPos.getX(); i <= blockPos2.getX(); ++i) {
+                            for (int j = blockPos.getY(); j <= blockPos2.getY(); ++j) {
+                                for (int k = blockPos.getZ(); k <= blockPos2.getZ(); ++k) {
+                                    mutable.set(i, j, k);
+                                    BlockState blockState = entity.world.getBlockState(mutable);
+                                    try {
+                                        if (Main.CONFIG_MANAGER.getCurrentFireLogic() == FireLogic.PERSISTENT) {
+                                            if (client.world.getBlockState(mutable).getBlock() instanceof SoulFireBlock) {
+                                                ((OnSoulFireAccessor)entity).setRenderSoulFire(true);
+                                            }
+                                            if (client.world.getBlockState(mutable).getBlock() instanceof FireBlock) {
+                                                ((OnSoulFireAccessor)entity).setRenderSoulFire(false);
+                                            }
+                                            if (entity.isInLava()) {
+                                                ((OnSoulFireAccessor)entity).setRenderSoulFire(false);
+                                            }
+                                        }
+                                        if (Main.CONFIG_MANAGER.getCurrentFireLogic() == FireLogic.CONSISTENT) {
+                                            if(entity.isInLava()) {
+                                                isSoulFire = false;
+                                                break outerLoop;
+                                            }
+                                            if (client.world.getBlockState(mutable).getBlock() instanceof SoulFireBlock) {
+                                                isSoulFire = true;
+                                                break outerLoop;
+                                            }
+                                        }
+                                    }
+                                    catch (Throwable throwable) {
+                                        CrashReport crashReport = CrashReport.create(throwable, "Colliding entity with block");
+                                        throw new CrashException(crashReport);
+                                    }
+                                }
+                            }
+                        }
+                        if(CONFIG_MANAGER.getCurrentFireLogic() == FireLogic.CONSISTENT) {
+                            ((OnSoulFireAccessor) entity).setRenderSoulFire(isSoulFire);
+                        }
+                    }
+
+                });
+            }
+        });
     }
 }
