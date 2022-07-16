85ca4809a0e8ad2d687177db51a2ec44cdf8bcb8
Lollypoplove08 <rafferty.smith@hotmail.com>
2022-07-16 11:06:52 +0200

    Initial 1.19 Commit

---
+++ src/main/java/com/oscimate/oscimate_soulflame/NetherBiomeColors.java
@@ -0,0 +1,25 @@
+package com.oscimate.oscimate_soulflame;
+
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.GrassColors;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.level.ColorResolver;
+
+public class NetherBiomeColors {
+    public static final ColorResolver NETHER_COLOR = (biome, x, z) -> biome.getWaterColor();
+
+    public  NetherBiomeColors() {
+    }
+
+    private static int getColor(BlockRenderView world, BlockPos pos, ColorResolver resolver) {
+        return world.getColor(pos, resolver);
+    }
+
+    public static int getNetherColor(BlockRenderView world, BlockPos pos) {
+        return getColor(world, pos, NETHER_COLOR);
+    }
+
+}
