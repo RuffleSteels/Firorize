5a6c4417f2c7e230ba90ba825d3213e22e382761
Lollypoplove08 <rafferty.smith@hotmail.com>
2024-08-18 22:33:24 +0200

    No longer requires mod menu, can open config via keybind

--- src/main/java/com/oscimate/firorize/config/ConfigScreen.java
+++ src/main/java/com/oscimate/firorize/config/ConfigScreen.java
@@ -9,14 +9,16 @@ import net.minecraft.text.Text;
 
 public class ConfigScreen extends Screen {
     protected static final int buttonWidth = 130;
-    private final Screen parent;
+    private Screen parent = null;
     protected static final int windowHeight = 182;
 
     protected ConfigScreen(Screen parent) {
         super(Text.literal(""));
         this.parent = parent;
     }
-
+    public ConfigScreen() {
+        super(Text.literal(""));
+    }
     @Override
     protected void init() {
         this.addDrawableChild(new ButtonWidget.Builder(Text.translatable("firorize.config.button.changeFireHeightScreen"), button -> this.client.setScreen(new ChangeFireHeightScreen(this))).dimensions(width / 2 + buttonWidth/2 - 40, height/2 - 15 - 20, buttonWidth, 20).build());
@@ -45,7 +47,11 @@ public class ConfigScreen extends Screen {
     }
 
     public void onClose() {
-        client.setScreen(parent);
+        if (parent == null) {
+            super.close();
+        } else {
+            client.setScreen(parent);
+        }
     }
 
     @Override
